be8c44dfdbdd803e2f0c22fe7ac22bef72cbd497
Rainer Kraft <rainer@mybuilder.com>
2020-12-15 13:51:08 +0000

    Add Day 05 Part 2

--- Day_05/day05.py
+++ Day_05/day05.py
@@ -9,4 +9,22 @@ for ticket in tickets:
     col = int(ticket[-3:].replace('R', '1').replace('L', '0'), 2)
     seatIds.append(row * 8 + col)
 
-print(max(seatIds))
+seatIds.sort()
+
+# Part 1
+print('Part 1 - Highest seat id: %d' % (seatIds[-1]))
+
+startId = seatIds[0]
+counter = 0
+for seatId in seatIds:
+    expectedSeatId = startId + counter
+    if (
+        seatId != expectedSeatId and
+        (expectedSeatId in seatIds) == False and
+        (expectedSeatId + 1) in seatIds and
+        (expectedSeatId - 1) in seatIds
+    ):
+        # Part 2
+        print('Part 2 - My seat id: %d' % (expectedSeatId))
+        break
+    counter += 1
